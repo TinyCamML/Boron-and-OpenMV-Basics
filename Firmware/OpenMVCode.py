ff6c6bacbdd455c84222d39618bb9cb1f5856758
Bentley Settin <bfs7357@uncw.edu>
2024-02-05 14:04:17 -0500

    Updated Code

--- Firmware/OpenMVCode.py
+++ Firmware/OpenMVCode.py
@@ -1,78 +1,86 @@
-# TinyFloodCamML OpenMV code - By: ebgoldstein - Mon Oct 2nd 2023
 
 import sensor
 import image
 import time
+import utime
 import utime
 import pyb
 import tf
 import machine
 
 from pyb import UART
-
-#uart = machine.UART(3, baudrate=9600)
-uart = UART(1, 9600) # UART3, adjust baudrate as needed
-#uart.init(9600, bits=8 , parity=None , stop=1, timeout_char=1000)
-
-# Uncomment if you have an LCD
-#import LCD
+from pyb import Pin, ExtInt
 
 
-#setup LEDs and set into known off state
+uart = machine.UART(1, baudrate=9600)
+uart = UART(1, 9600) # UART1, adjust baudrate as needed
+
+#rtc = pyb.RTC()
+
+
 redLED   = pyb.LED(1)
 
 sensor.reset() # Initialize the camera sensor.
 sensor.set_pixformat(sensor.RGB565) # or sensor.GRAYSCALE
 sensor.set_framesize(sensor.QVGA)
-# Uncomment if you have an LCD
-#sensor.set_framesize(sensor.QQVGA2) # Special 128x160 framesize for LCD Shield.
+
 sensor.skip_frames(time = 2000)
-# Uncomment if you have an LCD
-#lcd.init() # Initialize the lcd screen.
+
 
 #red light during setup
 redLED.on()
 
 
-#Load the TFlite model and the labels
+#Load the TFlite model and the labels, takes a lot of power.
 net = tf.load('MNv2Flood_cat (3).tflite', load_to_fb=True)
 labels = ['Flood', 'NoFlood']
 
 #turn led off when model is loaded
 redLED.off()
 
+def callback(line):
+    pass
 
-#MAIN LOOP
+led = pyb.LED(3)
+pin = Pin("P7", Pin.IN, Pin.PULL_UP)
+ext = ExtInt(pin, ExtInt.IRQ_FALLING, Pin.PULL_UP, callback)
+
 
 while(True):
 
+    machine.sleep()
+
+    #check for sign of life.
     img = sensor.snapshot()
 
-    #Do the classification and get the object returned by the inference.
     TF_objs = net.classify(img)
-    print(TF_objs)
 
-    #The object has a output, which is a list of classifcation scores
-    #for each of the output channels. this model only has 2 (flood, no flood).
     Flood = TF_objs[0].output()[0]
     NoFlood = TF_objs[0].output()[1]
-
-
-    #This loop just prints to the serial terminal (and optionally the LCD screen),
-    # but you could also blink an LED, write a number (or datetime), etc.
 
     if Flood > NoFlood:
         print('Flood')
         uart.write('Flood')
-        #uart.read('Flood)
-        # Uncomment if you have an LCD
-        # img.draw_string(1,140, "Flood", color = (10,10,100), scale = 2,mono_space = False)
+
     else:
         print('No Flood')
         uart.write('No Flood')
-        #uart.read('No Flood)
-        # Uncomment if you have an LCD
-        # img.draw_string(1,140, "No Flood", color = (10,10,100), scale = 2,mono_space = False)
 
-    # Uncomment if you have an LCD
-    #lcd.display(img) # dsiplay image
+
+    led.on()
+    time.sleep_ms(100)
+    led.off()
+    time.sleep_ms(100)
+
+
+
+
+
+
+
+
+
+
+
+
+
